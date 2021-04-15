1ea38a2629012bc0b13acafa14da1b06b9c31305
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2021-04-15 17:47:30 +0200

    base projet

--- Front-end/JS/script.js
+++ Front-end/JS/script.js
@@ -1,4 +1,30 @@
-let azerty = document.querySelector('#test');
-fetch('http://localhost:3000/api/cameras')
-.then(response => response.json()
-.then(response => azerty.setAttribute('src',response[0].imageUrl)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let azerty = document.querySelector('#test');
+// fetch('http://localhost:3000/api/cameras')
+// .then(response => response.json()
+// .then(response => azerty.setAttribute('src',response[0].imageUrl)));
