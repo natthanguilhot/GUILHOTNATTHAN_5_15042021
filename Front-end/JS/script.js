7eecd7e5a061a82dfe7c413d9e65d2b45a5d2781
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2021-04-16 12:49:25 +0200

    summary
    début scss, debut requete fetch avec affichage en html

--- Front-end/JS/script.js
+++ Front-end/JS/script.js
@@ -1,30 +1,19 @@
+let photoZurss = document.querySelector('#photo_zurss');
+fetch('http://localhost:3000/api/cameras')
+.then(response => response.json()
+.then(response => photoZurss.setAttribute('src',response[0].imageUrl)));
 
+let nomZurss = document.querySelector('#nom_zurss');
+fetch('http://localhost:3000/api/cameras')
+.then(response => response.json()
+.then(response => nomZurss.innerHTML = response[0].name));
 
+let descriptionZurss = document.querySelector('#description_zurss');
+fetch('http://localhost:3000/api/cameras')
+.then(response => response.json()
+.then(response => descriptionZurss.innerHTML = response[0].description));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let azerty = document.querySelector('#test');
-// fetch('http://localhost:3000/api/cameras')
-// .then(response => response.json()
-// .then(response => azerty.setAttribute('src',response[0].imageUrl)));
+let prixZurss = document.querySelector('#prix_zurss');
+fetch('http://localhost:3000/api/cameras')
+.then(response => response.json()
+.then(response => prixZurss.innerHTML = response[0].price));
