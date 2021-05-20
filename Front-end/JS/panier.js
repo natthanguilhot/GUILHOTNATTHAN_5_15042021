21e6a950066ae0dc2f4697462b7093a37ee4e936
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2021-05-20 17:48:36 +0200

    début refac

--- Front-end/JS/panier.js
+++ Front-end/JS/panier.js
@@ -9,73 +9,23 @@ fetch("http://localhost:3000/api/cameras")
 .then(response => {
     let productInCart = [];
     productInCart = JSON.parse(localStorage.getItem('produit'));
-    for(let product of productInCart) {
-        prixPanier = product.prix + prixPanier;
-        // Copie du template et ajout du clone au noeud parent
-        let newCarteProdPanier = carteProdPanier.cloneNode(true);
-        parentNodePanier.appendChild(newCarteProdPanier);
-        newCarteProdPanier.removeAttribute('id');
-        newCarteProdPanier.classList.remove('hidden');
-        let btnSuppr = newCarteProdPanier.querySelector('.supprimer_produit_panier');
-        btnSuppr.dataset.id = dataId+1;
-        dataId += 1;
-        let imgProduit = newCarteProdPanier.querySelector('.img_produit');
-        let nomProduit = newCarteProdPanier.querySelector('.nom_produit');
-        let optionProduit = newCarteProdPanier.querySelector('.option_produit');
-        let qteProduit = newCarteProdPanier.querySelector('.qte_produit');
-        let prixProduit = newCarteProdPanier.querySelector('.prix_produit');
-        let price = new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(product.prix/100);
-        nomProduit.innerHTML = product.nom;
-        optionProduit.innerHTML = product.option;
-        prixProduit.innerHTML = price;
-        qteProduit.innerHTML = product.qty;
-        //
-        
-        // Afficher l'image lié à l'id du produit
-        let productId = product.id;
-        for(let product of response) {
-            if(productId === product._id) {
-                imgProduit.setAttribute('src', product.imageUrl);
-            }
-        }
-        //
-    }
-    // Affichage du prix
-    let livraison = 499;
-    let prixTotalPanier = prixPanier + livraison;
-    let prixPanierHTML = document.querySelector('#prix_panier');
-    let prixTotalHTML = document.querySelector('#prix_total');
-    let pricePanier = new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(prixPanier/100); 
-    let priceTotalPanierConvert = new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(prixTotalPanier/100); 
-    prixPanierHTML.innerHTML = pricePanier;
-    prixTotalHTML.innerHTML = priceTotalPanierConvert;
-    //
 
-    // Suppresion du produit 
-    let allBtnSuppr = document.querySelectorAll('.supprimer_produit_panier');
-    for(let btn of allBtnSuppr) {
-        btn.addEventListener('click', function() {
-            let idData = this.parentNode.dataset.id;
-            productInCart.splice([idData],1);
-            localStorage.setItem('produit', JSON.stringify(productInCart));
-            window.location.reload(); // TODO mettre fonction recharger panier et non la page
-        });    
-    }
-    //
+    affichageProdPanier(response);
+    
 
     // Formulaire de commande
     let btnFormulaireCommande = document.querySelector('#btncommande');
     btnFormulaireCommande.addEventListener('click', function(){
         let nomForm = document.querySelector('#lastname').value;
         let prenomForm = document.querySelector('#firstname').value;
-        let adresseFrom = document.querySelector('#adresse').value;
+        let adresseForm = document.querySelector('#adresse').value;
         let villeForm = document.querySelector('#city').value;
         let mailForm = document.querySelector('#mail').value;
         let contact = [];
         let formContact = {
             firstName : prenomForm,
             lastName : nomForm,
-            address : adresseFrom,
+            address : adresseForm,
             city : villeForm,
             email : mailForm,
         }
@@ -88,31 +38,7 @@ fetch("http://localhost:3000/api/cameras")
             products : tablProductId,
         };
 
-        function nomPrenomVilleControl() {
-           if(/^[A-Za-z\s]{2,20}$/.test(nomForm) && /^[A-Za-z]{2,20}$/.test(prenomForm) && /^[A-Za-z]{2,20}$/.test(villeForm)) {
-            return true;
-           }else {
-            alert("Les champs Nom, Prénom et Ville ne doivent contenir que des lettres et pas d'espace inutile.")
-            return false;
-           }
-        };
-        function emailControl () {
-            if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(mailForm)){
-                return true;
-            } else {
-                alert('Email incorrect');
-                return false;
-            }
-        }
-        function addressControl () {
-            if(/^[A-Za-z0-9\s]{5,50}$/.test(villeForm)) {
-                return true;
-            } else {
-                alert('Adresse incorrect');
-                return false;
-            }
-        }
-        if(nomPrenomVilleControl() && emailControl() && addressControl()) { // on controle le formulaire, Si formulaire ok traitement ci dessous, sinon les fonctions envoie une alerte
+        if(nomPrenomVilleControl(nomForm, prenomForm, villeForm) && emailControl(mailForm) && addressControl(adresseForm)) { // on controle le formulaire, Si formulaire ok traitement ci dessous, sinon les fonctions envoie une alerte
             contact.push(formContact); // on push le formulaire dans le tableau contact
             fetch("http://localhost:3000/api/cameras/order", {
             method: 'POST',
@@ -133,11 +59,9 @@ fetch("http://localhost:3000/api/cameras")
                 order.push(orderResume); // on push l'objet dans le tableau 
                 localStorage.setItem('order', JSON.stringify(order)); // on envoie le tableau dans le LS
             });
-            localStorage.clear('produit'); // on vide le panier LS
+            localStorage.removeItem('produit'); // on vide le panier LS
             window.location.href = "confirmation.html"; // on envoie sur la page confirmation    
-        } else {
-            console.log("Formulaire incorrect");
-        }
+        } 
         event.preventDefault();      
     });        
 }));
--- Front-end/JS/script.js
+++ Front-end/JS/script.js
@@ -1,9 +1,9 @@
 // Affichage nombre produit dans le panier
-// TODO Juste afficher le nombre d'élément dans le panier
-let numberOfProductInCart = document.querySelector('#number_prod_in_cart');
-productInCart = [];
-productInCart = JSON.parse(localStorage.getItem('produit'));
 function totalProductInCart() {
+    let numberOfProductInCart = document.querySelector('#number_prod_in_cart');
+    productInCart = [];
+    productInCart = JSON.parse(localStorage.getItem('produit'));
+
     if (productInCart === null) {
         numberOfProductInCart.innerHTML = '0';
     }else {
@@ -12,9 +12,11 @@ function totalProductInCart() {
     }    
 }
 totalProductInCart();
-
+//
+//
 function affichageConfirmationCommande () {
     let orderResume = JSON.parse(localStorage.getItem("order"));
+    console.log(orderResume);
     let nameResumeHTML = document.querySelector('#name');
     let numberOfProductResumeHTML = document.querySelector('#nbr_produit');
     let orderIdHTML = document.querySelector('#order_id');
@@ -25,3 +27,93 @@ function affichageConfirmationCommande () {
         localStorage.clear();
     },1)
 }
+function affichageProdPanier(response) {
+
+    for(let product of productInCart) {
+        prixPanier = product.prix + prixPanier;
+        // Copie du template et ajout du clone au noeud parent
+        let newCarteProdPanier = carteProdPanier.cloneNode(true);
+        parentNodePanier.appendChild(newCarteProdPanier);
+        newCarteProdPanier.removeAttribute('id');
+        newCarteProdPanier.classList.remove('hidden');
+        let btnSuppr = newCarteProdPanier.querySelector('.supprimer_produit_panier');
+        btnSuppr.dataset.id = dataId+1;
+        dataId += 1;
+        let imgProduit = newCarteProdPanier.querySelector('.img_produit');
+        let nomProduit = newCarteProdPanier.querySelector('.nom_produit');
+        let optionProduit = newCarteProdPanier.querySelector('.option_produit');
+        let qteProduit = newCarteProdPanier.querySelector('.qte_produit');
+        let prixProduit = newCarteProdPanier.querySelector('.prix_produit');
+        let price = new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(product.prix/100);
+        nomProduit.innerHTML = product.nom;
+        optionProduit.innerHTML = product.option;
+        prixProduit.innerHTML = price;
+        qteProduit.innerHTML = product.qty;
+        //
+        
+        // Afficher l'image lié à l'id du produit
+        let productId = product.id;
+        for(let product of response) {
+            if(productId === product._id) {
+                imgProduit.setAttribute('src', product.imageUrl);
+            }
+        }
+        //
+    }
+    // Suppresion du produit 
+    dataId = -1;
+    let allBtnSuppr = document.querySelectorAll('.supprimer_produit_panier');
+    for(let btn of allBtnSuppr) {
+        btn.addEventListener('click', function() {
+            let idData = this.dataset.id;
+            console.log(idData);
+            productInCart.splice([idData],1);
+            localStorage.setItem('produit', JSON.stringify(productInCart));
+            let allDivProduit = document.querySelectorAll('.produit');
+            for(let div of allDivProduit) {
+                div.parentElement.removeChild(div);        
+            }
+            affichageProdPanier(response);
+            totalProductInCart();
+        });    
+    }
+    //
+
+    // Affichage du prix
+    let livraison = 499;
+    let prixTotalPanier = prixPanier + livraison;
+    let prixPanierHTML = document.querySelector('#prix_panier');
+    let prixTotalHTML = document.querySelector('#prix_total');
+    let pricePanier = new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(prixPanier/100); 
+    let priceTotalPanierConvert = new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(prixTotalPanier/100); 
+    prixPanierHTML.innerHTML = pricePanier;
+    prixTotalHTML.innerHTML = priceTotalPanierConvert;
+    //
+    
+}
+
+// Fonctions validation formulaire
+function nomPrenomVilleControl(nomForm, prenomForm, villeForm) {
+    if(/^[A-Za-z\s]{2,20}$/.test(nomForm) && /^[A-Za-z]{2,20}$/.test(prenomForm) && /^[A-Za-z]{2,20}$/.test(villeForm)) {
+     return true;
+    }else {
+     alert("Les champs Nom, Prénom et Ville ne doivent contenir que des lettres et pas d'espace inutile.")
+     return false;
+    }
+ };
+ function emailControl (email) {
+    if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+        return true;
+    } else {
+        alert('Email incorrect');
+        return false;
+    }
+ }
+ function addressControl (adresseForm) {
+    if(/^[A-Za-z0-9-é\s]{5,50}$/.test(adresseForm)) {
+        return true;
+    } else {
+        alert('Adresse incorrect');
+        return false;
+    }
+ }
