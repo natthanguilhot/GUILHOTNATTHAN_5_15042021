2266cf2ee48ca916fe745a2072edaecb2ff5d3ba
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2021-05-27 16:38:58 +0200

    upd

--- Front-end/JS/product.js
+++ Front-end/JS/product.js
@@ -1,114 +1,12 @@
-import {totalProductInCart,affichageProduit,checkProdInCart} from './script.js';
-
+import {totalProductInCart,affichageProduit, produit} from './script.js';
 totalProductInCart();
-
-
 // Récupération de l'id du produit via l'url
 const url = new URL(window.location.href);
 const id = url.searchParams.get('id');
 //
-
 // Requête au produit correspondant à l'id et affichage de celui ci
 fetch("http://localhost:3000/api/cameras/"+ id)
 .then(response => response.json()
 .then(response => {
-    affichageProduit(response);
-    // Gestion de la quantité
-    let qtyPlus = document.querySelector('#qtyplus');
-    let qtyMinus = document.querySelector('#qtyminus');
-    let inputQty = document.querySelector('#qty');
-    let numberQty = 1;
-    inputQty.setAttribute('value',numberQty);
-
-    qtyMinus.addEventListener('click', function() {
-        inputQty.value = numberQty-1;
-        numberQty = numberQty-1;
-    });
-    qtyPlus.addEventListener('click', function() {
-        inputQty.value = numberQty+1;
-        numberQty = numberQty+1;
-    });
-    //
-
-
-    // Gestion du panier 
-    //Récupérer contenu localstorage avec getItem (JSON.parse) 
-    //Si localstorage vide, alors nouveau panier vide
-    let addToCart = document.querySelector('button#addtocart');
-    let selectOption = document.querySelector('#length');
-    let errAddCart = document.querySelector('#erreuraddcart');
-    let confirmAddCart = document.querySelector('#confirmaddcart');
-
-    addToCart.addEventListener('click', function() {
-        let productInCart = [];
-        productInCart = JSON.parse(localStorage.getItem('produit'));    
-        // Si option est === 'Choississez votre lentille' alors popup erreur ('Veuillez sélectionner une lentille !')
-        if (selectOption.value === 'Choississez votre lentille') {
-            errAddCart.classList.remove('hidden');
-            errAddCart.classList.add('anim_erreur_lentille');
-        } else { // Sinon si il y a deja des produits dans le panier, alors on push le produit dans tableau 'productInCart' et on l'insert dans local storage
-            if(productInCart) {
-                //Si productInCart/LS pas vide
-                let product = {
-                    id : response._id,
-                    qty : Number(inputQty.value),
-                    nom : response.name,
-                    prix : response.price*Number(inputQty.value),
-                    option : selectOption.value
-                }
-                // Verif si id et option du produit sont deja présent, si oui, on change la quantité
-                let isProductInCart = false;
-                checkProdInCart(product);
-                //
-                // Si produit est déjà présent, alors on ajoute la quantité de l'input à la quantité déjà présente
-                if (isProductInCart === true) {
-                    localStorage.setItem('produit', JSON.stringify(productInCart));
-                } else {
-                    // Sinon push le produit dans le tableau productInCart et l'insérer dans le LS
-                    productInCart.push(product);
-                    localStorage.setItem('produit', JSON.stringify(productInCart));
-    
-                    //animation erreur ou validation d'ajout au panier
-                    errAddCart.classList.add('hidden');
-                    confirmAddCart.classList.remove('hidden');
-                    confirmAddCart.classList.add('anim_add_cart_ok');
-                    setTimeout(function() {
-                        confirmAddCart.classList.add('hidden');
-                    },3000);
-    
-    
-                    //Affichage nombre d'article dans panier
-                    let numberOfProductInCart = document.querySelector('#number_prod_in_cart');
-                    productInCart = JSON.parse(localStorage.getItem('produit'));
-                    numberOfProductInCart.innerHTML = productInCart.length;    
-                }
-
-            } else { // Sinon si LocalStorage vide, on récupère le tableau 'productInCart', on push le produit dans le tableau et on met a jour le LocalStorage
-                productInCart = [];
-                let product = {
-                    id : response._id,
-                    qty : Number(inputQty.value),
-                    nom : response.name,
-                    prix : response.price*Number(inputQty.value),
-                    option : selectOption.value
-                }
-                productInCart.push(product);
-                localStorage.setItem('produit', JSON.stringify(productInCart));
-
-                //animation erreur ou validation d'ajout au panier
-                errAddCart.classList.add('hidden');
-                confirmAddCart.classList.remove('hidden');
-                confirmAddCart.classList.add('anim_add_cart_ok');
-                setTimeout(function() {
-                    confirmAddCart.classList.add('hidden');
-                },3000);
-
-
-                //Affichage nombre d'article dans panier
-                let numberOfProductInCart = document.querySelector('#number_prod_in_cart');
-                productInCart = JSON.parse(localStorage.getItem('produit'));
-                numberOfProductInCart.innerHTML = productInCart.length;
-            }    
-        }
-    })
+    produit(response);
 }));
--- Front-end/JS/script.js
+++ Front-end/JS/script.js
@@ -20,6 +20,13 @@ export function totalProductInCart() {
     }    
 };
 //
+// Fonction formatage des prix
+function prixConvert (prixamodifer) {
+    return new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(prixamodifer/100);
+};
+//
+
+/////////////////////////////////////////  PAGE INDEX
 // Affichage produits page index
 export function affichageProduitsIndex (response) {
     for(let product of response) {
@@ -52,10 +59,147 @@ export function affichageProduitsIndex (response) {
     }
 };
 //
+////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////// PAGE PRODUIT
+export function affichageProduit(response){
+    let imgProduct = document.querySelector('img');
+    let nomProduct = document.querySelector('.nom_produit')
+    let descriptionProduct = document.querySelector('.description_produit');
+    let prixProduct = document.querySelector('.prix_produit');
+    nomProduct.innerHTML = response.name;
+    descriptionProduct.innerHTML = response.description
+
+    let price = new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(response.price/100);
+    prixProduct.innerHTML = price;
+
+    imgProduct.setAttribute('src', response.imageUrl);
+
+    let title = document.querySelector('title');
+    title.innerHTML = 'Orinoco - ' + response.name;
+
+    for (let length of response.lenses) {
+        let selectLength = document.querySelector('#length');
+        const newOptionLength = document.createElement('option')
+        newOptionLength.setAttribute('value', length);
+        newOptionLength.innerHTML = length;
+        selectLength.appendChild(newOptionLength);
+    }
+};
+export function produit (response) {
+    affichageProduit(response);
+    // Gestion de la quantité
+    let qtyPlus = document.querySelector('#qtyplus');
+    let qtyMinus = document.querySelector('#qtyminus');
+    let inputQty = document.querySelector('#qty');
+    let numberQty = 1;
+    inputQty.setAttribute('value',numberQty);
+    qtyMinus.addEventListener('click', function() {
+        inputQty.value = numberQty-1;
+        numberQty = numberQty-1;
+    });
+    qtyPlus.addEventListener('click', function() {
+        inputQty.value = numberQty+1;
+        numberQty = numberQty+1;
+    });
+    //
+    // Gestion du panier 
+    //Récupérer contenu localstorage avec getItem (JSON.parse) 
+    //Si localstorage vide, alors nouveau panier vide
+    let addToCart = document.querySelector('button#addtocart');
+    let selectOption = document.querySelector('#length');
+    let errAddCart = document.querySelector('#erreuraddcart');
+    let confirmAddCart = document.querySelector('#confirmaddcart');
+    addToCart.addEventListener('click', function() {
+        let productInCart = [];
+        productInCart = JSON.parse(localStorage.getItem('produit'));    
+        // Si option est === 'Choississez votre lentille' alors popup erreur ('Veuillez sélectionner une lentille !')
+        if (selectOption.value === 'Choississez votre lentille') {
+            errAddCart.classList.remove('hidden');
+            errAddCart.classList.add('anim_erreur_lentille');
+        } else { // Sinon si il y a deja des produits dans le panier, alors on push le produit dans tableau 'productInCart' et on l'insert dans local storage
+            if(productInCart) {
+                //Si productInCart/LS pas vide
+                let product = {
+                    id : response._id,
+                    qty : Number(inputQty.value),
+                    nom : response.name,
+                    prix : response.price*Number(inputQty.value),
+                    option : selectOption.value
+                }
+                // Verif si id et option du produit sont deja présent, si oui, on change la quantité
+                let isProductInCart = false;
+                function checkProdInCart () {
+                    for (let i of productInCart) {
+                        if (product.id === i.id && product.option === i.option) {
+                            isProductInCart = true;
+                            let qtyOfProductInCart;
+                            qtyOfProductInCart = i.qty;
+                            i.qty = Number(qtyOfProductInCart) + Number(inputQty.value);
+                            i.prix = response.price * i.qty;
+                            // Anim validation d'ajout
+                            confirmAddCart.classList.remove('hidden');
+                            confirmAddCart.classList.add('anim_add_cart_ok');
+                            setTimeout(function() {
+                                confirmAddCart.classList.add('hidden');
+                            },3000);        
+                            break;
+                        }
+                    }
+                };                
+                checkProdInCart();
+                //
+                // Si produit est déjà présent, alors on ajoute la quantité de l'input à la quantité déjà présente
+                if (isProductInCart === true) {
+                    localStorage.setItem('produit', JSON.stringify(productInCart));
+                } else {
+                    // Sinon push le produit dans le tableau productInCart et l'insérer dans le LS
+                    productInCart.push(product);
+                    localStorage.setItem('produit', JSON.stringify(productInCart));
+    
+                    //animation erreur ou validation d'ajout au panier
+                    errAddCart.classList.add('hidden');
+                    confirmAddCart.classList.remove('hidden');
+                    confirmAddCart.classList.add('anim_add_cart_ok');
+                    setTimeout(function() {
+                        confirmAddCart.classList.add('hidden');
+                    },3000);
+                    //Affichage nombre d'article dans panier
+                    let numberOfProductInCart = document.querySelector('#number_prod_in_cart');
+                    productInCart = JSON.parse(localStorage.getItem('produit'));
+                    numberOfProductInCart.innerHTML = productInCart.length;    
+                }
+            } else { // Sinon si LocalStorage vide, on récupère le tableau 'productInCart', on push le produit dans le tableau et on met a jour le LocalStorage
+                productInCart = [];
+                let product = {
+                    id : response._id,
+                    qty : Number(inputQty.value),
+                    nom : response.name,
+                    prix : response.price*Number(inputQty.value),
+                    option : selectOption.value
+                }
+                productInCart.push(product);
+                localStorage.setItem('produit', JSON.stringify(productInCart));
+                //animation erreur ou validation d'ajout au panier
+                errAddCart.classList.add('hidden');
+                confirmAddCart.classList.remove('hidden');
+                confirmAddCart.classList.add('anim_add_cart_ok');
+                setTimeout(function() {
+                    confirmAddCart.classList.add('hidden');
+                },3000);
+                //Affichage nombre d'article dans panier
+                let numberOfProductInCart = document.querySelector('#number_prod_in_cart');
+                productInCart = JSON.parse(localStorage.getItem('produit'));
+                numberOfProductInCart.innerHTML = productInCart.length;
+            }    
+        }
+    })    
+};
+////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////// PAGE PANIER
 // Controle formualaire et envoie
-// TODO découper la fonction en 3/4 autre fonctions
-function getDataForm (productInCart) {
+function getDataForm () {
     let adresseForm = document.querySelector('#adresse').value;
     let mailForm = document.querySelector('#mail').value;
     let villeForm = document.querySelector('#city').value;
@@ -63,8 +207,7 @@ function getDataForm (productInCart) {
     let prenomForm = document.querySelector('#firstname').value;
     return {address : adresseForm, mail : mailForm, ville : villeForm, nom : nomForm, prenom : prenomForm};
 };
-function checkForm () {
-    let returnDataForm = getDataForm();
+function checkForm (returnDataForm) {
     if(nomPrenomVilleControl(returnDataForm.nom, returnDataForm.prenom, returnDataForm.ville) && emailControl(returnDataForm.mail) && addressControl(returnDataForm.address)) { // on controle le formulaire, Si formulaire ok traitement ci dessous, sinon les fonctions envoie une alerte
         createArrayForAPI(); // On crée les tableaux réquis par l'API
         sendOrder(); // On envoie le tableau a à l'API
@@ -119,30 +262,11 @@ export function formulaireCommande () {
     // Formulaire de commande
     let btnFormulaireCommande = document.querySelector('#btncommande');
     btnFormulaireCommande.addEventListener('click', function(){
+        let returnDataForm = getDataForm();
         getDataForm(productInCart); // On récupère les input du formulaire
-        checkForm(); // On vérifié les inputs et si c'est ok, on lance les autres fonctions
+        checkForm(returnDataForm); // On vérifié les inputs et si c'est ok, on lance les autres fonctions
         event.preventDefault();      
     });        
-};
-//
-
-
-//
-export function affichageConfirmationCommande () {
-    let orderResume = JSON.parse(localStorage.getItem("order"));
-    let nameResumeHTML = document.querySelector('#name');
-    let numberOfProductResumeHTML = document.querySelector('#nbr_produit');
-    let orderIdHTML = document.querySelector('#order_id');
-    nameResumeHTML.innerHTML = orderResume[0].prenom;
-    numberOfProductResumeHTML.innerHTML = orderResume[0].orderLength;
-    orderIdHTML.innerHTML = orderResume[0].orderId;
-    setTimeout(function(){
-        localStorage.removeItem('order');
-    },500)
-};
-
-function prixConvert (prixamodifer) {
-    return new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(prixamodifer/100);
 };
 export function affichageProdPanier(response, productInCart) {
     for(let product of productInCart) {
@@ -234,48 +358,18 @@ function nomPrenomVilleControl(nomForm, prenomForm, villeForm) {
     }
  };
 //
-// Page produit
-export function affichageProduit(response){
-    let imgProduct = document.querySelector('img');
-    let nomProduct = document.querySelector('.nom_produit')
-    let descriptionProduct = document.querySelector('.description_produit');
-    let prixProduct = document.querySelector('.prix_produit');
-    nomProduct.innerHTML = response.name;
-    descriptionProduct.innerHTML = response.description
 
-    let price = new Intl.NumberFormat('fr-FR', {style :'currency', currency :'EUR', minimumFractionDigits : 2}).format(response.price/100);
-    prixProduct.innerHTML = price;
-
-    imgProduct.setAttribute('src', response.imageUrl);
-
-    let title = document.querySelector('title');
-    title.innerHTML = 'Orinoco - ' + response.name;
-
-    for (let length of response.lenses) {
-        let selectLength = document.querySelector('#length');
-        const newOptionLength = document.createElement('option')
-        newOptionLength.setAttribute('value', length);
-        newOptionLength.innerHTML = length;
-        selectLength.appendChild(newOptionLength);
-    }
+//////////////////////////////////////////////////// PAGE CONFIRMATION
+export function affichageConfirmationCommande () {
+    let orderResume = JSON.parse(localStorage.getItem("order"));
+    let nameResumeHTML = document.querySelector('#name');
+    let numberOfProductResumeHTML = document.querySelector('#nbr_produit');
+    let orderIdHTML = document.querySelector('#order_id');
+    nameResumeHTML.innerHTML = orderResume[0].prenom;
+    numberOfProductResumeHTML.innerHTML = orderResume[0].orderLength;
+    orderIdHTML.innerHTML = orderResume[0].orderId;
+    setTimeout(function(){
+        localStorage.removeItem('order');
+    },500)
 };
-
 //
-export function checkProdInCart (product) {
-    for (let i of productInCart) {
-        if (product.id === i.id && product.option === i.option) {
-            isProductInCart = true;
-            let qtyOfProductInCart;
-            qtyOfProductInCart = i.qty;
-            i.qty = Number(qtyOfProductInCart) + Number(inputQty.value);
-            i.prix = response.price * i.qty;
-            // Anim validation d'ajout
-            confirmAddCart.classList.remove('hidden');
-            confirmAddCart.classList.add('anim_add_cart_ok');
-            setTimeout(function() {
-                confirmAddCart.classList.add('hidden');
-            },3000);        
-            break;
-        }
-    }
-}
